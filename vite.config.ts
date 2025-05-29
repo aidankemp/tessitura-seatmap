96437dc90712cfe54a97d37837147f90d6a5de89
akemp-dcs <aidankemp37@gmail.com>
2025-05-29 23:43:20 +1000

    Removed unnecessary files from distributed library package

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,12 @@ import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
+  publicDir: false,
   plugins: [
     react(),
     dts({
       tsconfigPath: "./tsconfig.app.json",
+      exclude: ["**/*.stories.tsx", "**/fixtures/**"],
     }),
   ],
   build: {
@@ -18,10 +20,26 @@ export default defineConfig({
       fileName: "index",
     },
     rollupOptions: {
-      external: ["react"],
+      external: [
+        "@mantine/core",
+        "@mantine/hooks",
+        "@mantine/notifications",
+        "@tabler/icons-react",
+        "react",
+        "react-dom",
+        "react-svg-seatmap",
+        "**/*.stories.tsx",
+        "**/fixtures/**",
+      ],
       output: {
         globals: {
           react: "React",
+          "react-dom": "ReactDOM",
+          "@mantine/core": "MantineCore",
+          "@mantine/hooks": "MantineHooks",
+          "@mantine/notifications": "MantineNotifications",
+          "@tabler/icons-react": "TablerIconsReact",
+          "react-svg-seatmap": "ReactSvgSeatmap",
         },
       },
     },
